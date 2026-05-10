d74b336b8b5a0c86969aa7615225d3b130509844
EliotTtg <cristophertaguada03@gmail.com>
2026-05-10 18:54:58 -0500

    [ADD] Conexion Base de datos

---
+++ src/config/ConexionDB.java
@@ -0,0 +1,23 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/sistema_inventario?useSSL=false&serverTimezone=UTC";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "mysqladmin";
+
+    public static Connection conectar() throws SQLException {
+
+        return DriverManager.getConnection(
+                URL,
+                USER,
+                PASSWORD
+        );
+    }
+}
--- src/main/App.java
+++ src/main/App.java
@@ -1,7 +1,22 @@
 package main;
 
+import config.ConexionDB;
+
+import java.sql.Connection;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        try (
+                Connection con = ConexionDB.conectar()
+        ) {
+
+            System.out.println("Conexion exitosa a MySQL");
+
+        } catch (Exception e) {
+
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
